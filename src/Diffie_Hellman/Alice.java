bf5536e69afaa5bcc713bf60154d3bed341322ce
JasonUrban <markjasonurban@gmail.com>
2018-11-16 14:10:54 +0300

    Initial commit

---
+++ src/Diffie_Hellman/Alice.java
@@ -0,0 +1,96 @@
+package Diffie_Hellman;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.net.*;
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class Alice {
+    private static boolean isPrime(long n) {
+        if (n % 2L == 0L) {
+            return false;
+        }
+        for (long i = 3L; i * i <= n; i += 2L) {
+            if (n % i == 0L)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] arg) {
+        ServerSocket serverSocket = null;
+        Socket clientAccepted = null;
+        ObjectInputStream sois = null;
+        ObjectOutputStream soos = null;
+        int p, g, a;
+        System.out.println("Generate random parameters or input them? 1 - input, else - random.");
+        Scanner scanner = new Scanner(System.in);
+        char choice = scanner.nextLine().charAt(0);
+        switch (choice) {
+            case '1':
+                while (true) {
+                    System.out.println("Input parameters p, g and secret key a:");
+                    scanner = new Scanner(System.in);
+                    p = scanner.nextInt();
+                    g = scanner.nextInt();
+                    a = scanner.nextInt();
+                    if(a <= 0 || g <= 0) {
+                        System.out.println("a and g must be positive!");
+                        continue;
+                    }
+                    if(a >= p - 1 || g >= p - 1) {
+                        System.out.println("a and g must be less than p - 1!");
+                        continue;
+                    }
+                    if (isPrime(p) && isPrime(g) && isPrime((p - 1) / 2)) {
+                        break;
+                    } else {
+                        System.out.println("p, (p - 1) / 2 and g must be prime numbers!");
+                    }
+                }
+                break;
+            default:
+                do {
+                    SecureRandom random = new SecureRandom();
+                    p = random.nextInt(1000000);
+                    g = random.nextInt(p - 1);
+                    a = random.nextInt(p - 1);
+                } while (!isPrime(p) || !isPrime(g) || !isPrime((p - 1) / 2));
+        }
+        try {
+            System.out.println("Alice listening....");
+            serverSocket = new ServerSocket(2525);
+            clientAccepted = serverSocket.accept();
+            System.out.println("Contact established....");
+            sois = new ObjectInputStream(clientAccepted.getInputStream());
+            soos = new ObjectOutputStream(clientAccepted.getOutputStream());
+            int A = new BigInteger(Integer.toString(g)).modPow(new BigInteger(Integer.toString(a)), new BigInteger(Integer.toString(p))).intValue();
+            soos.writeObject(new int[]{p, g, A});
+            System.out.println("Sent p, g and A values: " + p + ", " + g + ", " + A);
+            int B = (int) sois.readObject();
+            System.out.println("Received B: " + B);
+            int key = new BigInteger(Integer.toString(B)).modPow(new BigInteger(Integer.toString(a)), new BigInteger(Integer.toString(p))).intValue();
+            System.out.println("Key received: " + key);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (sois != null) {
+                    sois.close();
+                }
+                if (soos != null) {
+                    soos.close();
+                }
+                if (clientAccepted != null) {
+                    clientAccepted.close();
+                }
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/Diffie_Hellman/Bob.java
@@ -0,0 +1,56 @@
+package Diffie_Hellman;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.net.*;
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] arg) {
+        try {
+            System.out.println("Contacting Alice....");
+            Socket clientSocket = new Socket("127.0.0.1", 2525);
+            System.out.println("Contact established....");
+            ObjectOutputStream coos = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream cois = new ObjectInputStream(clientSocket.getInputStream());
+            int[] values = (int[]) cois.readObject();
+            int p = values[0], g = values[1], A = values[2], b;
+            System.out.println("Received p, g and A values: " + p + ", " + g + ", " + A);
+            System.out.println("Generate secret key or input it? 1 - input, else - random.");
+            Scanner scanner = new Scanner(System.in);
+            char choice = scanner.nextLine().charAt(0);
+            switch (choice) {
+                case '1':
+                    while (true) {
+                        System.out.println("Input parameters p, g and secret key a:");
+                        scanner = new Scanner(System.in);
+                        b = scanner.nextInt();
+                        if (b <= 0) {
+                            System.out.println("a and g must be positive!");
+                            continue;
+                        }
+                        if (b >= p - 1) {
+                            System.out.println("b must be less than p - 1!");
+                            continue;
+                        }
+                        break;
+                    }
+                    break;
+                default:
+                    SecureRandom random = new SecureRandom();
+                    b = random.nextInt(p - 1);
+            }
+            int B = new BigInteger(Integer.toString(g)).modPow(new BigInteger(Integer.toString(b)), new BigInteger(Integer.toString(p))).intValue();
+            coos.writeObject(B);
+            System.out.println("Sent B value: " + B);
+            int key = new BigInteger(Integer.toString(A)).modPow(new BigInteger(Integer.toString(b)), new BigInteger(Integer.toString(p))).intValue();
+            System.out.println("Key received: " + key);
+            coos.close();
+            cois.close();
+            clientSocket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
